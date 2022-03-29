41d0e38eb7c8686436d2b0b9176fbab5f0de27ea
Seubmarine <seubmarinepro@gmail.com>
2022-03-29 12:55:49 +0200

    better system to prevent unnessery rotation in stack a and b

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 15:33:04 by tbousque          #+#    #+#             */
-/*   Updated: 2022/03/28 22:50:05 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/03/28 23:44:17 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,17 +119,19 @@ void	quick_sort_b(t_stack *a, t_stack *b, int len)
 	}
 	quick_sort_a(a, b, a_len);
 	i = 0;
-	while (i < len - a_len)
+	if (num_pos(b, len - a_len) != num(b))
 	{
-		rot_rev(b);
-		i++;
+		while (i < len - a_len)
+		{
+			rot_rev(b);
+			i++;
+		}
 	}
 	quick_sort_b(a, b, len - a_len);
 }
 
 void	quick_sort_a(t_stack *a, t_stack *b, int len)
 {
-	static int is_first_pass = 1;
 	int	i;
 	int b_len;
 	const int median = get_median(a, len);
@@ -137,10 +139,7 @@ void	quick_sort_a(t_stack *a, t_stack *b, int len)
 	i = 1;
 	b_len = 0;
 	if (len <= 3)
-	{
-		is_first_pass = 0;
 		return ;
-	}
 	while (i <= len && a && num(a) != num_pos(a, 1))
 	{
 		if (num(a) > median)
@@ -153,10 +152,13 @@ void	quick_sort_a(t_stack *a, t_stack *b, int len)
 		i++;
 	}
 	i = 0;
-	while (i < len - b_len && !is_first_pass)
+	if (num_pos(a, len - b_len) != num(a))
 	{
-		rot_rev(a);
-		i++;
+		while (i < len - b_len)
+		{
+			rot_rev(a);
+			i++;
+		}
 	}
 	quick_sort_a(a, b, len - b_len);
 	quick_sort_b(a, b, b_len);
@@ -185,7 +187,7 @@ int	main(int argc, char **argv)
 	if (stack_a.list)
 	{	
 		algo(&stack_a, &stack_b, argc - 1);
-		print_stack(stack_a, stack_b);
+		//print_stack(stack_a, stack_b);
 	}
 	free(m_list_array);
 	return (0);
