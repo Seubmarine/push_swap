a930f7e532137685740ce96998ccdee5afbd1036
Seubmarine <seubmarinepro@gmail.com>
2022-07-17 14:29:40 +0200

    norm libft.h

--- ft_std/libft.h
+++ ft_std/libft.h
@@ -6,12 +6,13 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 03:33:40 by tbousque          #+#    #+#             */
-/*   Updated: 2022/03/08 03:46:53 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/07/17 05:21:16 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+
 long long int	ft_strtoll(const char *nptr, char **endptr, int base);
 #endif
