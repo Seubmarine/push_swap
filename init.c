98e143cc9ccd6881eb554790eebc524d2b0dc9b7
Seubmarine <seubmarinepro@gmail.com>
2022-07-18 18:36:20 +0200

    bad num_count after changing argv in create_and_verify_list

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 15:33:04 by tbousque          #+#    #+#             */
-/*   Updated: 2022/07/18 18:18:23 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/07/18 18:33:05 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,9 @@ void	free_print_error_and_exit(void *to_free)
 	exit(EXIT_FAILURE);
 }
 
-t_list_double	*create_and_verify_list(int argc, char **argv)
+t_list_double	*create_and_verify_list(int argc, char **argv, \
+	const size_t num_count)
 {
-	const size_t	num_count = get_total_arg_count(argc - 1, argv + 1);
 	char			**char_list;
 	t_list_double	*m_list_array;
 
@@ -83,17 +83,18 @@ t_list_double	*create_and_verify_list(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
+	const size_t	num_count = get_total_arg_count(argc - 1, argv + 1);
 	t_list_double	*m_list_array;
 	t_stack			stack_a;
 	t_stack			stack_b;
 	t_op_vector		m_vec_op;
 
-	m_list_array = create_and_verify_list(argc, argv);
+	m_list_array = create_and_verify_list(argc, argv, num_count);
 	op_vector_init(&m_vec_op, 16, m_list_array);
 	stack_a = stack_init('a', &m_vec_op);
 	stack_b = stack_init('b', &m_vec_op);
 	stack_a.list = m_list_array;
-	quick_sort_a(&stack_a, &stack_b, get_total_arg_count(argc - 1, argv + 1));
+	quick_sort_a(&stack_a, &stack_b, num_count);
 	op_vector_applyf(&m_vec_op, &op_print);
 	op_vector_free(&m_vec_op);
 	free(m_list_array);
